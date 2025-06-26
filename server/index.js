ab0425685e9359387912ce0c76f14fb306945862
KingAbeni <graceabenimoi@gmail.com>
2025-06-26 09:47:34 +0300

    settinng server using nodemon and socket.io

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
-const ws = require('ws')
-const server=new ws.Server( {port : '3000'} )
+import { createServer } from 'http'
+import { Server} from 'socket.io'
 
-server.on('connection', socket => {
+const httpServer = createServer()
+
+const io=new Server(httpServer, {
+    cors:{
+        origin: process.env.NODE_ENV === 'production' ? false : ["http://localhost:5500"]
+    }
+})
+
+io.on('connection', socket => {
     socket.on('message',message => {
+        console.log(`User ${socket.id} connected`)
+
         const b=Buffer.from(message)
         console.log(b.toString)
         socket.send(`${message}`)
     })
 })
+
+httpServer.listen(3000)
